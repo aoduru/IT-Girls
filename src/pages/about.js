a20b12f7f878bac01cd847fd306edd00b44db974
cdvines <cdvines@iu.edu>
2021-02-06 20:17:44 -0500

    update about page with team

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,89 @@
 import React from 'react';
+// This page has bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import "../styles/about.css";
 
+import placeholderimage from "../images/image.jpeg";
+import camisa from "../images/camisa.jpg";
+import uju from "../images/uju.jpg";
 
 class About extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    // }
-
     render() {
         return (
             <>
-                <div className="flex flex-col container bg-gray-100">                
-                    <h1>About</h1>
+                <div className="flex flex-col about-container">
+                    <div className="about-section containers">
+                        <h1>About</h1>
+
+
+                        <h2>The Problem</h2>
+                        <div className="row">
+                        </div>
+
+                        <div className="about-section-divider"></div>
+                        <h2>Our Solution</h2>
+                        <div className="row">
+                        </div>
+
+
+
+
+
+                        <div className="about-section-divider"></div>
+                        <h2>Meet the Team</h2>
+                        <div className="row">
+
+                            {/* Team Member 1 */}
+                            <div className="col-sm-6 col-lg-3">
+                                <div className="card">
+                                    <img className="card-img-top" src={placeholderimage} alt="Shanice" />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Shanice</h5>
+                                        <p class="card-text">bio text.</p>
+                                        <a href="#" class="btn btn-primary">Shanice's LinkedIn</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Team Member 2 */}
+                            <div className="col-sm-6 col-lg-3">
+                                <div className="card">
+                                    <img className="card-img-top" src={uju} alt="Uju" />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Uju</h5>
+                                        <p class="card-text">bio text.</p>
+                                        <a href="#" class="btn btn-primary">Uju's LinkedIn</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Team Member 3 */}
+                            <div className="col-sm-6 col-lg-3">
+                                <div className="card">
+                                    <img className="card-img-top" src={placeholderimage} alt="Yesenia" />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Yesenia</h5>
+                                        <p class="card-text">bio text.</p>
+                                        <a href="#" class="btn btn-primary">Yesenia's LinkedIn</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {/* Team Member 4 */}
+                            <div className="col-sm-6 col-lg-3">
+                                <div className="card">
+                                    <img className="card-img-top" src={camisa} alt="Camisa" />
+                                    <div class="card-body">
+                                        <h5 class="card-title">Camisa</h5>
+                                        <p class="card-text">bio text.</p>
+                                        <a href="#" class="btn btn-primary">Camisa's LinkedIn</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>                
+
+
                 </div>
             </>
         )
--- src/pages/home.js
+++ src/pages/home.js
@@ -9,16 +9,21 @@ class Home extends React.Component {
     render() {
         return (
             <>
-                <div className="home-hero flex">
-                    <div className="container">
+            <div className="row">
+                <div className="col-xs-12 col-sm-6 col-md-4 home-hero">
+                    <div>
                         <h1>Covid-19</h1>
+                        <h2>Breaking down the Vaccine</h2>
                     </div>
                 </div>
 
+                {/* Hero Image Here */}
+                <div className="col-xs-12 col-sm-6 col-md-8 home-hero-img"></div>
+            </div>
 
-                <div className="home-section container">
-                    <h2>Don't be misinformed about the Vaccine</h2>
-                </div>
+            <div className="home-section container">
+                <h2>Be Informed</h2>
+            </div>
             </>
         )
     }
